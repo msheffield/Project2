e4e7ad1f050dd8c909a253f6e7dab60fcf639341
msheffield <msheffield@adaptivebiotech.com>
2019-12-09 21:22:03 -0800

    created a post route for creating an account

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,7 +8,16 @@ module.exports = function(app) {
     });
   });
 
-  // Create a new example
+  // Create a new account
+  app.post("/api/account", function(req, res) {
+    db.Post.create({
+      role: req.body.role
+    }).then(function(result) {
+      res.json(result);
+    });
+  });
+
+  // Create a new tutor
   app.post("/api/tutor", function(req, res) {
     db.Post.create({
       firstName: req.body.firstName,
