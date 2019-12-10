ff3f9ea859f72043f19c754b2ff0560290486ed1
msheffield <msheffield@adaptivebiotech.com>
2019-12-09 21:13:02 -0800

    created a post route for creating a tutor

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,30 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Example.findAll({}).then(function(result) {
+      res.json(result);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/tutor", function(req, res) {
+    db.Post.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+      location: req.body.location,
+      skillLevel: req.body.skillLevel,
+      description: req.body.description,
+      grade: req.body.grade
+    }).then(function(result) {
+      res.json(result);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(result) {
+      res.json(result);
     });
   });
 };
